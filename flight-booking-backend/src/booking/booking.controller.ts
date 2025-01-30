719779a3d3076dc671147ff9f15138f0d1fa7407
owalishawon <owali.shawon@gmail.com>
2025-01-30 15:58:24 +0600

    Update booking.controller.ts

--- flight-booking-backend/src/booking/booking.controller.ts
+++ flight-booking-backend/src/booking/booking.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, UseGuards } from '@nestjs/common';
 import { BookingService } from './booking.service';
+import { CreateBookingDto } from './dto/create-booking.dto';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
-@Controller('booking')
+@Controller('bookings')
+@UseGuards(JwtAuthGuard)
 export class BookingController {
-  constructor(private readonly bookingService: BookingService) {}
+  constructor(private bookingService: BookingService) {}
+
+  @Post()
+  createBooking(@Body() createBookingDto: CreateBookingDto) {
+    return this.bookingService.createBooking(createBookingDto);
+  }
+
+  @Get(':userId')
+  getBookingsByUserId(@Param('userId') userId: number) {
+    return this.bookingService.getBookingsByUserId(userId);
+  }
 }
