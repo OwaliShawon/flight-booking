78d16338f24ab36d707f6c1f3a4946e89ff43af2
owalishawon <owali.shawon@gmail.com>
2025-01-30 15:54:51 +0600

    Update flight.service.ts

--- flight-booking-backend/src/flight/flight.service.ts
+++ flight-booking-backend/src/flight/flight.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Flight } from './flight.entity';
+import { SearchFlightDto } from './dto/search-flight.dto';
 
 @Injectable()
-export class FlightService {}
+export class FlightService {
+  constructor(
+    @InjectRepository(Flight)
+    private flightRepository: Repository<Flight>,
+  ) {}
+
+  async searchFlights(searchFlightDto: SearchFlightDto): Promise<Flight[]> {
+    const { from, to, departureDate, returnDate } = searchFlightDto;
+    const query = this.flightRepository
+      .createQueryBuilder('flight')
+      .where('flight.from = :from', { from })
+      .andWhere('flight.to = :to', { to })
+      .andWhere('DATE(flight.departureTime) = :departureDate', {
+        departureDate,
+      });
+
+    if (returnDate) {
+      query.andWhere('DATE(flight.arrivalTime) = :returnDate', { returnDate });
+    }
+
+    return query.getMany();
+  }
+
+  async getFlightById(id: number): Promise<Flight> {
+    return this.flightRepository.findOne({ where: { id } });
+  }
+}
