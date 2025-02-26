645249c0edbfcc6dafdc494ceab4704fc55a5ab1
owalishawon <owali.shawon@gmail.com>
2025-02-26 10:46:42 +0600

    Update payment.module.ts

--- flight-booking-backend/src/payment/payment.module.ts
+++ flight-booking-backend/src/payment/payment.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { PaymentController } from './payment.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './payment.entity';
+import { User } from '../user/user.entity';
+import { Booking } from '../booking/booking.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Payment, User, Booking]), ConfigModule],
   controllers: [PaymentController],
   providers: [PaymentService],
+  exports: [PaymentService],
 })
 export class PaymentModule {}
