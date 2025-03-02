bc9b6800419bbd2df086cc849e78068763578c17
owalishawon <owali.shawon@gmail.com>
2025-03-02 14:31:06 +0600

    Update admin.module.ts

--- flight-booking-backend/src/admin/admin.module.ts
+++ flight-booking-backend/src/admin/admin.module.ts
@@ -6,9 +6,19 @@ import { Booking } from 'src/booking/booking.entity';
 import { User } from 'src/user/user.entity';
 import { Flight } from 'src/flight/flight.entity';
 import { Payment } from 'src/payment/payment.entity';
+import { AuthModule } from 'src/auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { FlightModule } from 'src/flight/flight.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Booking, User, Flight, Payment])],
+  imports: [
+    TypeOrmModule.forFeature([Booking, User, Flight, Payment]),
+    AuthModule,
+    JwtModule.register({}),
+    FlightModule,
+    UserModule,
+  ],
   providers: [DashboardService],
   controllers: [AdminController],
   exports: [DashboardService],
