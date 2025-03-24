0224692047b41a45fe1a3bd0a3a319ad0a54030e
owalishawon <owali.shawon@gmail.com>
2025-03-24 13:33:33 +0600

    Add Swagger for api documentation

--- flight-booking-backend/src/main.ts
+++ flight-booking-backend/src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Flight booking API')
+    .setDescription('The flight booking API description')
+    .setVersion('1.0')
+    .addTag('flight-booking')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(PORT, () => {
     Logger.log('Server is running on http://localhost:' + PORT, 'Bootstrap');
   });
